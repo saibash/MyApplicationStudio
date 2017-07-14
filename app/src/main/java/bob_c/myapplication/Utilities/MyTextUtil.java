decf80a3e68e4223986424b2fdfaf69de7c12015
saibash <jhonatan.contrerasd@gmail.com>
2017-07-14 11:40:39 -0500

    Initial commit

---
+++ app/src/main/java/bob_c/myapplication/Utilities/MyTextUtil.java
@@ -0,0 +1,17 @@
+package bob_c.myapplication.Utilities;
+
+/**
+ * Created by Jhonatan Contreras on 12/07/2017.
+ */
+
+public class MyTextUtil {
+
+    private String name;
+
+    public MyTextUtil(String name) {
+        this.name = name;
+    }
+
+    public MyTextUtil() {
+    }
+}
